0b9c83a55eaf4a5dc74a34c243cb02e8c359d042
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2022-12-10 20:23:45 +0700

    This is our project about fashion

---
+++ assets/JS/javascript.js
@@ -0,0 +1,12 @@
+
+
+  document.getElementById("banner_left").addEventListener("mouseover", function(){
+    document.getElementById("background").style.backgroundImage ="linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url(assets/img/slider_1.webp)"
+  });
+  
+  document.getElementById("banner_middle").addEventListener("mouseover", function(){
+    document.getElementById("background").style.backgroundImage ="linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url(assets/img/slider_2.webp)"
+  });
+  document.getElementById("banner_right").addEventListener("mouseover", function(){
+    document.getElementById("background").style.backgroundImage ="linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.5)),url(assets/img/slider_3.webp)"
+  });
